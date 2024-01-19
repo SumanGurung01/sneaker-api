1f4aec755ed8584434e5ee6c86ed77d20606f674
Suman Gurung <suman_201900250@smit.smu.edu.in>
2024-01-20 02:26:49 +0530

    add : new sneakers

--- data.js
+++ data.js
@@ -188,6 +188,215 @@ const sneakers = [
         released: "06-01-2023",
         brand: "Adidas",
         id: 5553749222
+    },
+    {
+        name: "Air Jordan 1 Mid Patent SE 'Black Gold'",
+        image: [
+            "https://image.goat.com/transform/v1/attachments/product_template_pictures/images/094/662/608/original/483514_00.png",
+            "https://image.goat.com/transform/v1/attachments/product_template_additional_pictures/images/079/297/927/original/483514_08.jpg.jpeg",
+            "https://image.goat.com/transform/v1/attachments/product_template_additional_pictures/images/079/297/921/original/483514_06.jpg.jpeg",
+            "https://image.goat.com/transform/v1/attachments/product_template_additional_pictures/images/079/297/918/original/483514_04.jpg.jpeg"
+        ],
+        color: "Black",
+        sizes: [3, 5, 6, 7, 9, 10, 13, 14, 17, 18],
+        silhouette: "Air Jordan 1",
+        description:
+            "The Air Jordan 1 Mid Patent SE ‘Black Gold’ showcases familiar design ingredients from previously released Air Jordan 1s: namely, the patent leather and metallic gold accents. The former appears on the overlays that contrast a simple white leather base. The latter appears on the Swoosh and branding elements that adorn the shoe’s collar, tongue tag, and sockliner.",
+        price: 120,
+        gender: ["men"],
+        released: "01-24-2019",
+        brand: "Nike",
+        id: 4751471657
+    },
+    {
+        name: "Air Jordan 1 Retro High OG 'Yellow Ochre'",
+        image: [
+            "https://image.goat.com/transform/v1/attachments/product_template_pictures/images/096/513/736/original/1276120_00.png.png",
+            "https://image.goat.com/transform/v1/attachments/product_template_additional_pictures/images/096/513/725/original/1276120_08.jpg.jpeg",
+            "https://image.goat.com/transform/v1/attachments/product_template_additional_pictures/images/096/663/798/original/1276120_06.jpg.jpeg",
+            "https://image.goat.com/transform/v1/attachments/product_template_additional_pictures/images/096/513/732/original/1276120_04.jpg.jpeg"
+        ],
+        color: "Yellow",
+        sizes: [3, 4, 5, 6, 7, 8, 11, 12, 14, 16, 17, 18],
+        silhouette: "Air Jordan 1",
+        description:
+            "The Air Jordan 1 Retro High OG ‘Yellow Ochre’ sports a smooth leather upper, featuring a white base with mustard yellow overlays at the forefoot and heel. Contrasting black accents settle on the high-cut collar and Swoosh. A woven Nike Air tag decorates the breathable nylon tongue, while the retro Wings logo is imprinted on the lateral collar flap. Lightweight cushioning is provided by a polyurethane wedge, housed in a durable rubber cupsole and fitted with an encapsulated Air-sole unit in the heel.",
+        price: 170,
+        gender: ["men"],
+        released: "01-14-2024",
+        brand: "Nike",
+        id: 1869777437
+    },
+    {
+        name: "A Ma Maniére x Air Jordan 4 Retro 'Violet Ore'",
+        image: [
+            "https://image.goat.com/transform/v1/attachments/product_template_pictures/images/081/581/385/original/1015173_00.png.png",
+            "https://image.goat.com/transform/v1/attachments/product_template_additional_pictures/images/081/581/413/original/1015173_08.jpg.jpeg",
+            "https://image.goat.com/transform/v1/attachments/product_template_additional_pictures/images/081/581/409/original/1015173_06.jpg.jpeg",
+            "https://image.goat.com/transform/v1/attachments/product_template_additional_pictures/images/081/581/398/original/1015173_04.jpg.jpeg"
+        ],
+        color: "Purple",
+        sizes: [3, 4, 5, 6, 7, 9, 10, 11, 12, 14, 15, 17, 18],
+        silhouette: "Air Jordan 4",
+        description:
+            "The A Ma Maniére x Air Jordan 4 Retro ‘Violet Ore’ delivers an elevated take on the legacy silhouette. The Atlanta-based retailer dresses the mid-top in a muted purple nubuck upper, accented with tonal quarter panel netting and a decorative chrome rivet embedded in the lateral collar. Woven Flight tongue tags display mismatched Jumpman and A Ma Maniére branding, while molded black heel tabs feature a raised Nike Air logo. Inscribed across the interior side of each heel tab is the phrase ‘It’s not about the shoes … it’s about where you’re going.’ Quilted textile lines the collar and insole for a comfortable fit. The sneaker rides on a cream-colored polyurethane midsole with visible Air-sole cushioning in the heel.",
+        price: 270,
+        gender: ["men"],
+        released: "11-18-2022",
+        brand: "Nike",
+        id: 2471843988
+    },
+    {
+        name: "Paris Saint-Germain x Air Jordan 4 Retro 'Bordeaux'",
+        image: [
+            "https://image.goat.com/transform/v1/attachments/product_template_pictures/images/044/338/689/original/577878_00.png.png",
+            "https://image.goat.com/transform/v1/attachments/product_template_additional_pictures/images/044/338/694/original/577878_08.jpg.jpeg",
+            "https://image.goat.com/transform/v1/attachments/product_template_additional_pictures/images/042/836/259/original/577878_06.jpg.jpeg",
+            "https://image.goat.com/transform/v1/attachments/product_template_additional_pictures/images/042/836/270/original/577878_04.jpg.jpeg"
+        ],
+        color: "White",
+        sizes: [3, 4, 5, 6, 7, 8, 11, 12, 14, 16, 17, 18],
+        silhouette: "Air Jordan 4",
+        description:
+            "Celebrating Jordan Brand’s partnership with the French football club, the Paris Saint-Germain x Air Jordan 4 Retro ‘Bordeaux’ features color blocking that recalls the silhouette’s OG ‘Fire Red’ composition. The white leather upper is accented with black wings and Bordeaux detailing on the eyelets, interior tongue and heel, the latter marked with dual Jumpman and PSG branding. A black woven tab on the lateral forefoot is inscribed with ‘Paname,’ a slang term for the French capital.",
+        price: 450,
+        gender: ["men"],
+        released: "10-11-2020",
+        brand: "Nike",
+        id: 9795765242
+    },
+    {
+        name: "Yeezy Boost 350 V2 'Granite'",
+        image: [
+            "https://image.goat.com/transform/v1/attachments/product_template_pictures/images/080/255/684/original/1003886_00.png.png",
+            "https://image.goat.com/transform/v1/attachments/product_template_additional_pictures/images/091/746/901/original/1003886_08.jpg.jpeg",
+            "https://image.goat.com/transform/v1/attachments/product_template_additional_pictures/images/080/255/696/original/1003886_06.jpg.jpeg",
+            "https://image.goat.com/transform/v1/attachments/product_template_additional_pictures/images/080/255/692/original/1003886_04.jpg.jpeg"
+        ],
+        color: "Grey",
+        sizes: [3, 4, 5, 6, 7, 8, 10, 11, 12, 14, 16, 18],
+        silhouette: "Yeezy Boost 350",
+        description:
+            "The adidas Yeezy Boost 350 V2 ‘Granite’ douses the lifestyle silhouette in versatile neutral tones. Secured with traditional lace closure, the grey Primeknit upper features distinctive center stitching and a black side stripe marked with ‘SPLY-350’ branding. The minimalist build is mounted on a full-length Boost midsole, which is made visible through cut-outs in the grippy rubber outsole. Sidewalls are wrapped in semi-translucent TPU for improved durability and support.",
+        price: 215,
+        gender: ["men"],
+        released: "08-15-2023",
+        brand: "Adidas",
+        id: 1175424395
+    },
+    {
+        name: "Yeezy Boost 350 V2 'Slate'",
+        image: [
+            "https://image.goat.com/transform/v1/attachments/product_template_pictures/images/075/775/173/original/924555_00.png.png",
+            "https://image.goat.com/transform/v1/attachments/product_template_additional_pictures/images/075/775/168/original/924555_08.jpg.jpeg",
+            "https://image.goat.com/transform/v1/attachments/product_template_additional_pictures/images/075/775/166/original/924555_06.jpg.jpeg",
+            "https://image.goat.com/transform/v1/attachments/product_template_additional_pictures/images/075/775/174/original/924555_04.jpg.jpeg"
+        ],
+        color: "Cream",
+        sizes: [3, 5, 6, 8, 11, 12, 14, 16, 18],
+        silhouette: "Yeezy Boost 350",
+        description:
+            "The adidas Yeezy Boost 350 V2 ‘Slate’ complements the minimalist silhouette with a neutral two-tone color scheme. The sock-like upper utilizes tan Primeknit construction, highlighted by distinctive center stitching down the vamp. A contrasting black streak adorns the lateral side with signature ‘SPLY-350’ branding. The lightweight build rests on a full-length Boost midsole, wrapped in a semi-translucent TPU cage for improved durability and support.",
+        price: 210,
+        gender: ["men"],
+        released: "09-04-2022",
+        brand: "Adidas",
+        id: 9626345866
+    },
+    {
+        name: "New Balance 550 'White Team Red'",
+        image: [
+            "https://image.goat.com/transform/v1/attachments/product_template_pictures/images/048/707/967/original/708099_00.png.png",
+            "https://image.goat.com/transform/v1/attachments/product_template_additional_pictures/images/048/708/109/original/708099_08.jpg.jpeg",
+            "https://image.goat.com/transform/v1/attachments/product_template_additional_pictures/images/048/708/049/original/708099_06.jpg.jpeg",
+            "https://image.goat.com/transform/v1/attachments/product_template_additional_pictures/images/048/708/041/original/708099_04.jpg.jpeg"
+        ],
+        color: "Red",
+        sizes: [3, 5, 6, 7, 8, 9, 10, 11, 12, 14, 16, 17, 18],
+        silhouette: "New Balance 550",
+        description:
+            "The New Balance 550 ‘White Team Red’ showcases a simple palette on the vintage hoops shoe originally released in 1989. The low-top makes use of a layered white leather upper, accented with micro-perforated detailing and contrasting red accents throughout. The latter includes a raised ‘N’ logo on the quarter panel and a basketball graphic on the woven tongue. A durable rubber outsole maintains the throwback aesthetic, featuring flex grooves for a smooth ride.",
+        price: 70,
+        gender: ["men"],
+        released: "12-01-2020",
+        brand: "New Balance",
+        id: 6429998129
+    },
+    {
+        name: "New Balance 550 'Hemp Pack - Sea Salt'",
+        image: [
+            "https://image.goat.com/transform/v1/attachments/product_template_pictures/images/088/742/924/original/1170105_00.png.png",
+            "https://image.goat.com/transform/v1/attachments/product_template_additional_pictures/images/088/742/923/original/1170105_08.jpg.jpeg",
+            "https://image.goat.com/transform/v1/attachments/product_template_additional_pictures/images/088/742/917/original/1170105_06.jpg.jpeg",
+            "https://image.goat.com/transform/v1/attachments/product_template_additional_pictures/images/088/742/921/original/1170105_04.jpg.jpeg"
+        ],
+        color: "Tan",
+        sizes: [3, 4, 5, 6, 7, 8, 9, 10, 12, 14, 16, 17, 18],
+        silhouette: "New Balance 550",
+        description:
+            "Dropping as part of the two-piece ‘Hemp Pack,’ the New Balance 550 ‘Sea Salt’ brings together a refined palette and a breathable build. The upper is crafted from white hemp canvas with tonal suede detailing on the collar overlay and raised ‘N’ logo. The latter is outlined in black, matching the Flying NB heel graphic and the ‘550’ embroidered on the lateral forefoot. A vintage off-white finish is applied to the rubber cupsole, featuring stitched sidewalls and an exposed foam wedge on the medial side.",
+        price: 110,
+        gender: ["men"],
+        released: "06-02-2023",
+        brand: "New Balance",
+        id: 9756958768
+    },
+    {
+        name: "Air Jordan 3 Retro 'Midnight Navy'",
+        image: [
+            "https://image.goat.com/transform/v1/attachments/product_template_pictures/images/095/632/197/original/1268961_00.png.png",
+            "https://image.goat.com/transform/v1/attachments/product_template_additional_pictures/images/095/632/213/original/1268961_08.jpg.jpeg",
+            "https://image.goat.com/transform/v1/attachments/product_template_additional_pictures/images/095/632/210/original/1268961_06.jpg.jpeg",
+            "https://image.goat.com/transform/v1/attachments/product_template_additional_pictures/images/095/632/206/original/1268961_04.jpg.jpeg"
+        ],
+        color: "White",
+        sizes: [5, 6, 7, 8, 9, 10, 11, 12, 14, 16, 17, 18],
+        silhouette: "Air Jordan 3",
+        description:
+            "The Air Jordan 3 Retro ‘Midnight Navy’ features OG-inspired color blocking that recalls the iconic ‘White Cement’ colorway from 1988. The classic silhouette dons a white tumbled leather upper, bolstered with the shoe’s iconic elephant-print overlays at the forefoot and heel. A Jumpman logo in Midnight Navy embellishes the tongue and molded back tab. The mid-top rides on an Air-assisted polyurethane midsole, finished in white with a contrasting splash of navy at the heel.",
+        price: 180,
+        gender: ["men"],
+        released: "12-17-2023",
+        brand: "Nike",
+        id: 2722791413
+    },
+    {
+        name: "Air Jordan 3 Retro 'White Cement Reimagined'",
+        image: [
+            "https://image.goat.com/transform/v1/attachments/product_template_pictures/images/082/913/709/original/1101598_00.png.png",
+            "https://image.goat.com/transform/v1/attachments/product_template_additional_pictures/images/082/913/694/original/1101598_08.jpg.jpeg",
+            "https://image.goat.com/transform/v1/attachments/product_template_additional_pictures/images/082/913/696/original/1101598_06.jpg.jpeg",
+            "https://image.goat.com/transform/v1/attachments/product_template_additional_pictures/images/082/913/704/original/1101598_04.jpg.jpeg"
+        ],
+        color: "White",
+        sizes: [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 16, 17, 18],
+        silhouette: "Air Jordan 3",
+        description:
+            "The Air Jordan 3 Retro 'White Cement Reimagined' brings back one of the original colorways, celebrating the 35th anniversary of the AJ3. Designed by Tinker Hatfield, the Reimagined iteration is built to the original 1988 specs. Returning true to form, the white leather upper pairs with elephant print overlays on the heel and toe while hits of Varsity Red appear on the lace loops and the Jumpman logo on the tongue. Nike Air is emblazoned on the back heel tab, while a visible Air unit provides lightweight cushioning. A slight hint of yellowing appears on the back heel and midsole, giving the shoe a vintage look.",
+        price: 290,
+        gender: ["men"],
+        released: "03-12-2023",
+        brand: "Nike",
+        id: 1371581811
+    },
+    {
+        name: "Air Jordan 3 Retro 'Fire Red' 2022",
+        image: [
+            "https://image.goat.com/transform/v1/attachments/product_template_pictures/images/075/209/345/original/942086_00.png.png",
+            "https://image.goat.com/transform/v1/attachments/product_template_additional_pictures/images/075/209/333/original/942086_08.jpg.jpeg",
+            "https://image.goat.com/transform/v1/attachments/product_template_additional_pictures/images/075/209/335/original/942086_06.jpg.jpeg",
+            "https://image.goat.com/transform/v1/attachments/product_template_additional_pictures/images/075/209/340/original/942086_04.jpg.jpeg"
+        ],
+        color: "White",
+        sizes: [3, 4, 5, 6, 7, 8, 9, 12, 14, 16, 17, 18],
+        silhouette: "Air Jordan 3",
+        description:
+            "The 2022 edition of the Air Jordan 3 Retro ‘Fire Red’ brings back an OG colorway of the first Air Jordan model designed by Tinker Hatfield. Originally released in 1988, the sneaker carries a white leather upper with dark grey elephant-print overlays at the forefoot and heel. Contrasting crimson accents settle on the perforated leather collar, molded eyelets, and polyurethane midsole, featuring a visible Air-sole unit nestled in the heel. Matching hits of Fire Red distinguish the sneaker’s branding elements, including a Jumpman logo embroidered on the tongue and ‘Nike Air’ embossed on the heel.",
+        price: 260,
+        gender: ["men"],
+        released: "09-11-2022",
+        brand: "Nike",
+        id: 1193959197
     }
 ];
 
